bd0ba50c4c1c87217732babc0627c7d77dd2f9b1
pk504b <pk.504b@gmail.com>
2025-01-24 10:16:51 +0100

    add vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,9 @@
 import type { Config } from '@sveltejs/adapter-vercel';
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 export const config: Config = {
 	runtime: 'nodejs22.x'
 };
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
