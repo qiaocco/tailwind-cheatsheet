939a1c622c648555526fcf14adbae2790a6801c0
Prince <pk.504b@gmail.com>
2025-08-16 18:01:48 +0200

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { loadTwJson, twVersions } from "@/utils";
 import Toast from "@/components/Toast";
 import { TwJson } from "@/utils/types";
 import AdPopup from "@/components/AdPopup";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   const [currTw, setCurrTw] = useState(twVersions[0]);
@@ -21,6 +22,7 @@ export default function Home() {
   return (
     <div className="max-w-screen-2xl mx-auto p-4">
       <MasonryLayout currTw={currTw} setCurrTw={setCurrTw} twJson={jsonData} />
+      <Footer />
       <Toast copied={""} toastVisible={false} />
       <AdPopup />
     </div>
