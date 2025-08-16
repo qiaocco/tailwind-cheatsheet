bd93c7c76d6c8aa8d73494814b817f3c95ef0e32
Prince <pk.504b@gmail.com>
2025-08-16 16:30:00 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,37 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: "Tailwind CSS Cheatsheet",
+  description: "A quick reference guide to learn Tailwind CSS. Easily browse and search through all Tailwind utility classes and CSS properties on a single page.",
+  keywords: ["cheat", "sheet", "cheatsheet", "tailwind", "css", "classes", "utilities", "v4", "spa", "single page", "html", "javascript"],
+  openGraph: {
+    type: "website",
+    url: "https://tailwindcss.504b.cc/",
+    title: "Tailwind CSS Cheatsheet",
+    description: "A quick reference guide to learn Tailwind CSS. Easily browse and search through all Tailwind utility classes and CSS properties on a single page.",
+    siteName: "Tailwind CSS Cheatsheet",
+    images: [
+      {
+        url: "https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Tailwind CSS Cheatsheet",
+    description: "A quick reference guide to learn Tailwind CSS. Easily browse and search through all Tailwind utility classes and CSS properties on a single page.",
+    images: [
+      {
+        url: "https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
