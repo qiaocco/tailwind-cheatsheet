7a085311f0c9910dd851a091b298e675456e25bd
Prince <pk.504b@gmail.com>
2025-08-17 09:50:07 +0200

    remove base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/tailwind-cheatsheet",
+  basePath: "",
   images: {
     unoptimized: true,
   },
--- src/components/AdPopup.tsx
+++ src/components/AdPopup.tsx
@@ -25,7 +25,7 @@ export default function AdPopup() {
             <Image
               width={1080}
               height={1080}
-              src="/tailwind-cheatsheet/taillens_popup.png"
+              src="/taillens_popup.png"
               className="w-md rounded-lg"
               alt="taillens"
               priority={false}
